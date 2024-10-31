010f1ae55c058bbc0af7257fe4b2f81522ef5b9a
vergilsparda2 <igroraKinsin@gmail.com>
2024-10-31 16:41:49 +0300

    Add files via upload

---
+++ Cirkets.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cirkets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //выводит слово cirkets каждый кадр
+        print("crickets");
+    }
+}
