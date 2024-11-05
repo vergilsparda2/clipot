3948d3dc0c58db466cf101d7f802d2d999b58416
vergilsparda2 <igroraKinsin@gmail.com>
2024-11-05 16:43:25 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //Здоровье NPS
+    public int health = 5;
+
+    //уровень NPS
+    public int level = 1;
+
+    //скорость NPS
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = health + level;
+        print("здоровье:" + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+
+
+
+    }
+}
